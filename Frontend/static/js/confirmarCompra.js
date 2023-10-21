bf36db744736f7b431589344ffa0404f1606a569
Victoria <126226075+cvicky12@users.noreply.github.com>
2023-10-20 21:40:07 -0600

    Update confirmarCompra.js

--- Frontend/static/js/confirmarCompra.js
+++ Frontend/static/js/confirmarCompra.js
@@ -1,52 +1,52 @@
-        // Precios y subtotal inicial
-        const pricePerItem = 75.00; // Precio por artículo en quetzales
-        let quantity = 1;
-        let subtotal = pricePerItem;
+document.addEventListener('DOMContentLoaded', function() {
+    const camposAdicionalesContainer = document.getElementById('camposAdicionales');
+    const agregarCamposButton = document.getElementById('agregarCampos');
+    let contador = 1;
 
-        // Funciones JavaScript para controlar la cantidad y eliminar productos
-        function incrementQuantity(button) {
-            const quantityElement = button.parentNode.querySelector('span');
-            quantity += 1;
-            quantityElement.textContent = quantity;
-            updateSubtotal();
-            updateTotalPrice();
-        }
+    agregarCamposButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Evita el comportamiento predeterminado del botón
 
-        function decrementQuantity(button) {
-            const quantityElement = button.parentNode.querySelector('span');
-            if (quantity > 1) {
-                quantity -= 1;
-                quantityElement.textContent = quantity;
-                updateSubtotal();
-                updateTotalPrice();
-            }
-        }
+        // Crea un nuevo conjunto de campos
+        const nuevoCampos = document.createElement('div');
+        nuevoCampos.innerHTML = `
+            <div class="form-group">
+            <label for="tipoDescripcion${contador}">Tipo de Descripción</label>
+            <select class="form-control" id="tipoDescripcion${contador}" required>
+                <option value="Sabor">Sabor</option>
+                <option value="Relleno">Relleno</option>
+            </select>
+            </div>
+            <div class="form-group">
+                <label for="descripcion${contador}">Descripción</label>
+                <textarea class="form-control" id="descripcion${contador}" rows="4" placeholder="Agrega notas adicionales"></textarea>
+            </div>
+        `;
 
-        function updateSubtotal() {
-            const subtotalElement = document.getElementById('subtotalPrice');
-            const newSubtotal = quantity * pricePerItem;
-            subtotalElement.textContent = newSubtotal.toFixed(2);
-            subtotal = newSubtotal;
-        }
+        contador = contador + 1;
 
-        function updateTotalPrice() {
-            const totalPriceElement = document.getElementById('totalPrice');
-            totalPriceElement.textContent = subtotal.toFixed(2);
-        }
+        camposAdicionalesContainer.appendChild(nuevoCampos);
+    });
+});
 
-        function removeProduct(button) {
-            const listItem = button.parentNode;
-            listItem.parentNode.removeChild(listItem);
-            updateSubtotal();
-            updateTotalPrice();
-        }
+$(document).ready(function() {
+    $('form').submit(function(e) {
+        e.preventDefault();
 
-        function showPaymentMethod() {
-            document.getElementById('paymentMethod').style.display = 'block';
-            document.querySelector('.progress-bar').style.width = '50%';
-            document.querySelector('.confirm-button').style.display = 'block';
-        }
+        // Obtener los valores de los campos
+        const nombre = $('#nombre').val();
+        const sabor = $('#sabor').val();
+        const relleno = $('#ingredientes').val();
+        const tipoPastel = $('#tipoPastel').val();
+        const precio = $('#precio').val();
+        const tipoDescripcion = $('#tipoDescripcion').val();
+        const descripcion = $('#descripcion').val();
 
-        function finishPurchase() {
-            document.querySelector('.progress-bar').style.width = '100%';
-        }
+        axios.post('/api/crearPasteles', { nombre, sabor, relleno, tipoPastel, precio, tipoDescripcion, descripcion })
+        .then(response => {
+          console.log("todo bien");
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    });
+});
